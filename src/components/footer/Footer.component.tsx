b6cce24e318a034f72b55060a050b1360ad30d27
fauzani <muhammadfauzanisa@gmail.com>
2023-05-26 17:35:16 +0700

    add footer unfixed

---
+++ src/components/footer/Footer.component.tsx
@@ -0,0 +1,13 @@
+import {Box, Container} from '@mui/material'
+
+const Footer = () => {
+  return (
+    <Box sx={{bgcolor: 'primary.main'}}>
+      <Container maxWidth="xl">
+        <h1>hello this is footer</h1>{' '}
+      </Container>
+    </Box>
+  )
+}
+
+export default Footer
--- src/pages/home/Home.component.tsx
+++ src/pages/home/Home.component.tsx
@@ -1,37 +1,41 @@
 import {Box, Button, Container, Divider, Typography} from '@mui/material'
 import Galery from './section/Galery.component'
 import HowTo from './section/HowTo.component'
+import Footer from '../../components/footer/Footer.component'
 
 const Home = () => {
   return (
-    <Container maxWidth="xl">
-      <Box
-        sx={{
-          width: 1 / 2,
-          color: 'white',
-          minHeight: '90vh',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'start',
-          justifyContent: 'center',
-          gap: 6,
-        }}
-      >
-        <Typography variant="h1">
-          A litle care for your dream vehicle
-        </Typography>
-        <Typography variant="h5">
-          A litle care for your dream vehicle A litle care for your dream
-          vehicle
-        </Typography>
-        <Button variant="contained" size="large">
-          browse car
-        </Button>
-      </Box>
-      <Galery />
-      <Divider />
-      <HowTo />
-    </Container>
+    <>
+      <Container maxWidth="xl">
+        <Box
+          sx={{
+            width: 1 / 2,
+            color: 'white',
+            minHeight: '90vh',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'start',
+            justifyContent: 'center',
+            gap: 6,
+          }}
+        >
+          <Typography variant="h1">
+            A litle care for your dream vehicle
+          </Typography>
+          <Typography variant="h5">
+            A litle care for your dream vehicle A litle care for your dream
+            vehicle
+          </Typography>
+          <Button variant="contained" size="large">
+            browse car
+          </Button>
+        </Box>
+        <Galery />
+        <Divider />
+        <HowTo />
+      </Container>
+      <Footer />
+    </>
   )
 }
 
